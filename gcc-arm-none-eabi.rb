dad5da7deac1995b1035a0eea7314e781fe435fd
Rene Hopf <renehopf@mac.com>
2016-09-15 09:11:15 +0200

    Update gcc-arm-none-eabi.rb

--- gcc-arm-none-eabi.rb
+++ gcc-arm-none-eabi.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GccArmNoneEabi < Formula
   homepage 'https://launchpad.net/gcc-arm-embdded'
-  version '20160330'
+  version '20160622'
   url 'https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q2-update/+download/gcc-arm-none-eabi-5_4-2016q2-20160622-mac.tar.bz2'
-  sha1 '582896c9bfef99b55e652e96f63dc8d6ea3dfeea'
+  sha256 '582896c9bfef99b55e652e96f63dc8d6ea3dfeea'
 
   def install 
     ohai 'Copying binaries...'
